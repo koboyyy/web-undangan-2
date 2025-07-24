d8c9b384cea8f3975854ec397bd4a7102e2c9938
EVOC <evoc2119@gamil.com>
2025-07-24 08:42:35 +0700

    install supabase/supabase-js supabase/ssr create supabaseClient.js .env hooks.server.js

---
+++ src/lib/supabaseClient.js
@@ -0,0 +1,10 @@
+import { createClient } from "@supabase/supabase-js";
+import {
+  VITE_PUBLIC_SUPABASE_URL,
+  VITE_PUBLIC_SUPABASE_ANON_KEY,
+} from "$env/static/public";
+
+export const supabase = createClient(
+  VITE_PUBLIC_SUPABASE_URL,
+  VITE_PUBLIC_SUPABASE_ANON_KEY
+);
